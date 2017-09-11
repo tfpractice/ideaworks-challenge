288f686356dfebde841dbbb5a132001631a5270c
theory friction practice <jackson.brebnor@gmail.com>
2017-09-11 17:20:31 -0400

    images/links

--- src/components/links.js
+++ src/components/links.js
@@ -1,10 +1,46 @@
-const runway = {
-  img: '/images/runway.png',
+const exquisite = {
+  id: 0,
+  img: '/images/exquisite.png',
   date: 'FEBRUARY 10 2017',
   text: 'Bureaux exquisite delightful carefully curated soft power.',
   author: 'LOREM IPSUM',
 };
+const sharp = {
+  id: 1,
+  text: 'Sharp bureaux sleepy K-pop carefully curated.',
+  img: '/images/sharp.png',
+  date: 'FEBRUARY 2 2017',
+  author: 'LOREM IPSUM',
+};
+const moritz = {
+  id: 2,
+  text: 'St Moritz uniforms Beams.',
+  img: '/images/moritz.png',
+  date: 'JANUARY 27 2017',
+  author: 'LOREM IPSUM',
+};
 
-export const links = Array(6)
-  .fill(runway)
-  .map((e, id) => ({ id, ...e }));
+const esse = {
+  id: 3,
+  text: 'Esse airport veniam ryokan soft power.',
+  img: '/images/esse.png',
+  date: 'JANUARY 21 2017',
+  author: 'LOREM IPSUM',
+};
+const pop = {
+  id: 4,
+  text: 'K-pop extraordinary.',
+  img: '/images/wine.png',
+  date: 'JANUARY 18 2017',
+  author: 'LOREM IPSUM',
+};
+
+const art = {
+  id: 5,
+  text: 'Artisanal iconic cutting-edge business class.',
+  img: '/images/iconic.png',
+  date: 'JANUARY 12 2017',
+  author: 'LOREM IPSUM',
+};
+
+export const links = [ exquisite, sharp, moritz, esse, pop, art ];
